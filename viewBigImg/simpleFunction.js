e38edc20efcd0312b2547e67756663c6c774f723
oNora <e.dimchevska@gmail.com>
2013-05-26 16:26:22 +0300

    fix

--- viewBigImg/simpleFunction.js
+++ viewBigImg/simpleFunction.js
@@ -1,15 +1,15 @@
 //A function to view a picture in a different window and a bigger size
 
 window.onload = function(){
-	var $nodes = document.getElementsByTagName("a");
-	for (var $i = 0, $length = $nodes.length; $i < $length; $i++) {
-		$nodes[$i].onclick = function($e) {
+	var linkElements = document.getElementsByTagName("a");
+	for (var i = 0, length = linkElements.length; i < length; $i++) {
+		linkElements[i].onclick = function(e) {
 			window.open(
 				this.href,
-				"picture",
+				"_blank",
 				"scrollbars=no,width=300,height=280,resizable=yes"
 			);
-			$e? $e.preventDefault() : (window.event.returnValue = false);
+			e? e.preventDefault() : (window.event.returnValue = false);
 		};
 	}
 };
