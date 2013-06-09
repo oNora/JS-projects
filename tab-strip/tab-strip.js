7b0c9a6a799f0de58339005b640210d1974fb4b2
oNora <e.dimchevska@gmail.com>
2013-06-09 20:28:20 +0300

    new tasks

---
+++ tab-strip/tab-strip.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(){
+	function tab_clicks(){
+		$("ul.tab-strip").on('click', 'li a.tab-btn', function (e) {
+			e.preventDefault();
+			var parentClass = $(this).parent().attr('class');
+			var parentElement = $(this).parent();
+			if ( parentClass != "current"){
+				$('.current').removeClass('current');
+				parentElement.addClass('current');
+			}
+		});
+	}
+	tab_clicks();
+});
