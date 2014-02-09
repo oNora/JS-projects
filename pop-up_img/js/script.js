335721c310f24ce95854ccc785c2ca5e56be0d02
Eleonora Dimchevska <e.dimchevska@gmail.com>
2014-02-09 18:39:08 +0200

    changing of structure

--- pop-up_img/js/script.js
+++ pop-up_img/js/script.js
@@ -5,19 +5,19 @@ $(document).ready( function() {
 
 		$('body').append('<div id="popUp_overlay">'
 							+ '<div id="img_popUp" class="alignCenterImg_blog"><div id="cancelButton"><span  class="icon-close"></span>'
-							+ '</div><img src="'+imgPath+'"/><br/><br/><p class="imgText">'+imgText+'</p></div></div>');
+							+ '</div><img src="' + imgPath + '"/><br/><br/><p class="imgText">' + imgText + '</p></div></div>');
 
 		setTimeout( function() {
 			$('#img_popUp').each( function() {
 				$(this).css('left', ($(window).width() - $(this).outerWidth()) / 2 + 'px');
-			$(this).css('top', ($(window).height() - $(this).outerHeight()) / 2 + 'px');
+				$(this).css('top', ($(window).height() - $(this).outerHeight()) / 2 + 'px');
 			});
 			$('#img_popUp').show();
 		},100);
 
 		$('#popUp_overlay').each( function() {
-			$(this).css('width', $(document).width()+ 'px');
-			$(this).css('height', $(document).height()-100+ 'px');
+			$(this).css('width', $(document).width() + 'px');
+			$(this).css('height', $(document).height() - 100 + 'px');
 
 		});
 			$('body').on('click', '#cancelButton', function() {
